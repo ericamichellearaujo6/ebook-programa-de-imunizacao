3607e25a301608b6932ec81fd5b669a4b49cf9b3
Erica Araujo <ericamichellearaujo6@gmail.com>
2024-10-22 15:31:10 -0200

    modificação

--- contracapa.lua
+++ contracapa.lua
@@ -6,6 +6,19 @@ local scene = composer.newScene()
 local somBotao
 
 
+-- Função auxiliar para criar botões
+local function createButton(sceneGroup, imagePath, x, y, scaleX, scaleY, onTap)
+    local button = display.newImage(sceneGroup, imagePath)
+    button.x = x
+    button.y = y
+    button.xScale = scaleX or 1
+    button.yScale = scaleY or 1
+    if onTap then
+        button:addEventListener("tap", onTap)
+    end
+    return button
+end
+
 -- -----------------------------------------------------------------------------------
 -- Configurações da página
 -- -----------------------------------------------------------------------------------
@@ -20,36 +33,51 @@ function scene:create(event)
      local centerY = display.contentCenterY
  
      -- Adicionar imagem de fundo
-     local bg = display.newImageRect(sceneGroup, "assets/capa.png", 768, 1024)
+     local bg = display.newImageRect(sceneGroup, "assets/contracapa.png", 768, 1024)
      bg.x = centerX
      bg.y = centerY 
  
 
-    -- Adicionar botão 'home'
-    local btHome = display.newImage("assets/btHome.png")
-    btHome.x = display.contentWidth - 70 
-    btHome.y = display.contentHeight - 55 
-    btHome.xScale = 0.5
-    btHome.yScale = 0.5
+       -- Carregar o som do botão
+       somBotao = audio.loadSound("assets/som.mp3") 
 
-    -- Carregar o som do botão
-    somBotao = audio.loadSound("assets/som.mp3") 
-
-    -- Função para navegar para a próxima pagina
+    -- Função para navegar para a pagina incicial
     local function onNextTap(event)
-        audio.play(somBotao) 
-        composer.gotoScene("capa", { effect = "zoomInOut", time = 700 })
+        audio.play(somBotao)
+        composer.gotoScene("capa", { effect = "zoomInOut", time = 500 })
     end
 
-    -- Adicionar evento de toque no botão 'home'
-    btHome:addEventListener("tap", onNextTap)
+     -- Função para navegar para a pagina anterior 
+    local function onBackTap(event)
+        audio.play(somBotao)
+        composer.gotoScene("referencias", { effect = "slideRight", time = 500 })
+    end
+     
+------- add botoes
 
-    
+   -- Adicionar botão 'home'
+   local btProx = createButton(
+    sceneGroup,
+    "assets/btHome.png",
+    display.contentWidth - 70, 
+    display.contentHeight - 55, 
+    0.5, 
+    0.5, 
+    onNextTap 
+)
 
-    -- Adicionar todos os elementos ao grupo da cena
-    sceneGroup:insert(bg)
-    sceneGroup:insert(btHome)
-   
+-- Adicionar botão 'Voltar'
+local btVolt = createButton(
+    sceneGroup,
+    "assets/bt-voltar.png",
+    70, 
+    display.contentHeight - 55, 
+    0.5, 
+    0.5, 
+    onBackTap 
+)
+
+  
 end
 
 -- show
@@ -75,12 +103,11 @@ end
 -- destroy()
 function scene:destroy(event)
     local sceneGroup = self.view
-
- -- Libere o som ao destruir a cena
- if somBotao then
-     audio.dispose(somBotao)
-     somBotao = nil
-   end
+    
+    if somBotao then
+        audio.dispose(somBotao)
+        somBotao = nil
+    end
 end
 
 -- -----------------------------------------------------------------------------------
