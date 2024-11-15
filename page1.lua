9aa1592ed1213689f6bf78ae4d4a11b0c9bde1b9
Erica Araujo <ericamichellearaujo6@gmail.com>
2024-11-15 02:11:03 -0200

    correcao

--- page1.lua
+++ page1.lua
@@ -1,11 +1,11 @@
 -- Importa o módulo composer para gerenciamento de cenas
 local composer = require("composer")
-
--- Cria uma nova cena
 local scene = composer.newScene()
 
 -- Variável para armazenar o som
 local somBotao
+local somColisao
+local fimImagem = nil
 
 -- Função auxiliar para criar botões
 local function createButton(sceneGroup, imagePath, x, y, scaleX, scaleY, onTap)
@@ -20,84 +20,154 @@ local function createButton(sceneGroup, imagePath, x, y, scaleX, scaleY, onTap)
     return button
 end
 
--- -----------------------------------------------------------------------------------
--- Scene event functions
--- -----------------------------------------------------------------------------------
+-- Função para criar uma entidade flutuante ( vírus e bactérias)
+local function criarEntidade(sceneGroup, imagePath, posX, posY)
+    local entidade = display.newImage(sceneGroup, imagePath, 50, 50)
+    entidade.x = posX
+    entidade.y = posY
+    entidade.xScale = 0.2
+    entidade.yScale = 0.2
+    return entidade
+end 
 
--- create()
+-- Função de colisão 
+local function verificarColisao(zeGotinha, entidade, grupoEntidade, i)
+    if zeGotinha and entidade then 
+    local distX = zeGotinha.x - entidade.x
+    local distY = zeGotinha.y - entidade.y
+    local distancia = math.sqrt(distX ^ 2 + distY ^ 2)
+    local distanciaColisao = (zeGotinha.width * zeGotinha.xScale / 2) + (entidade.width * entidade.xScale / 2)
+
+ 
+
+
+    
+    if distancia < distanciaColisao then
+        -- Som só toca quando houver colisão
+        audio.play(somColisao)
+        entidade:removeSelf()
+        table.remove(grupoEntidade, i)
+
+        -- Verifica se todas as entidades foram destruídas
+        if #virusArray == 0 and #bacArray == 0 then
+           if not fimImagem then
+            print("Tentando carregar a imagem de vitória.")
+             fimImagem = display.newImage(sceneGroup, "assets/img.png", 200, 200)
+             fimImagem.x = display.contentCenterX
+             fimImagem.y = display.contentCenterY
+            end
+         end
+     end
+   end 
+end
+
+-- Função de atualização genérica para qualquer grupo de entidades
+local function atualizarColisao(zeGotinha)
+    -- Verifica as colisões com os vírus
+    for i = #virusArray, 1, -1 do
+        local virus = virusArray[i]
+        if virus then
+            verificarColisao(zeGotinha, virus, virusArray, i)
+        end
+    end
+
+    
+    -- Verifica as colisões com as bactérias
+    for i = #bacArray, 1, -1 do
+        local bac = bacArray[i]
+        if bac then
+            verificarColisao(zeGotinha,bac, bacArray, i)
+        end
+    end
+end
+
 function scene:create(event)
     local sceneGroup = self.view
-
-    -- Coordenadas para o centro da tela
     local centerX = display.contentCenterX
     local centerY = display.contentCenterY
 
     -- Adicionar imagem de fundo
-    local bg = display.newImageRect(sceneGroup, "assets/introducao1.png", 768, 1024)
+    local bg = display.newImageRect(sceneGroup, "assets/introducao.png", 768, 1024)
     bg.x = centerX
-    bg.y = centerY 
+    bg.y = centerY
 
-    -- Adicionar o ze gotinha 
- local zeGotinha = display.newImageRect(sceneGroup, "assets/gotinha.png", 100, 100) 
- zeGotinha.x = display.contentWidth - 690  
- zeGotinha.y = display.contentHeight - 200  
 
- -- Aumentar o tamanho do ze gotinha
- zeGotinha.xScale = 2.5  
- zeGotinha.yScale = 2.5  
+   
+    -- Adicionar o Zé Gotinha
+    local zeGotinha = display.newImageRect(sceneGroup, "assets/gotinha.png", 100, 100)
+    zeGotinha.x = display.contentWidth - 400
+    zeGotinha.y = display.contentHeight - 200
+    zeGotinha:scale(2.5, 2.5)
 
-  -- Adicionar o virus
-  local zeGotinha = display.newImageRect(sceneGroup, "assets/virus.png", 100, 100)
-  zeGotinha.x = display.contentWidth - 500  
-  zeGotinha.y = display.contentHeight - 200  
- 
-  -- Aumentar o tamanho do virus
-  zeGotinha.xScale = 0.9  
-  zeGotinha.yScale = 0.9  
-
-   -- Adicionar a bacteria 
- local zeGotinha = display.newImageRect(sceneGroup, "assets/bacteria.png", 100, 100)
- zeGotinha.x = display.contentWidth - 280  
- zeGotinha.y = display.contentHeight - 200  
-
- -- Aumentar o tamanho da bacteria
- zeGotinha.xScale = 0.9  
- zeGotinha.yScale = 0.9  
- 
-   -- Carregar o som do botão
-   somBotao = audio.loadSound("assets/som.mp3") 
-
-    -- Função para navegar para a próxima pagina
-    local function onNextTap(event)
-        audio.play(somBotao)
-        composer.gotoScene("page3", { effect = "slideLeft", time = 500 })
+    -- Função para arrastar o Zé Gotinha
+    local function arrastarImagem(event)
+        if event.phase == "began" then
+            display.getCurrentStage():setFocus(zeGotinha)
+            zeGotinha.isFocus = true
+            zeGotinha.offsetX = event.x - zeGotinha.x
+            zeGotinha.offsetY = event.y - zeGotinha.y
+        elseif event.phase == "moved" and zeGotinha.isFocus then
+            zeGotinha.x = event.x - zeGotinha.offsetX
+            zeGotinha.y = event.y - zeGotinha.offsetY
+        elseif event.phase == "ended" or event.phase == "cancelled" then
+            display.getCurrentStage():setFocus(nil)
+            zeGotinha.isFocus = false
+        end
+        return true
     end
 
-    -- Função para navegar para a pagina anterior 
+    zeGotinha:addEventListener("touch", arrastarImagem)
+
+    -- Grupos de vírus e bactérias
+    virusArray = {}
+    bacArray = {}
+
+    -- Posições para os vírus e bactérias
+    local posiVirus = { 
+        {x = 150, y = display.contentHeight - 190}, 
+        {x = 370, y = display.contentHeight - 450}, 
+        {x = 590, y = display.contentHeight - 250} 
+    }
+
+    local posiBac = {
+         {x = 130, y = display.contentHeight - 450}, 
+         {x = 540, y = display.contentHeight - 350},
+         {x = 630, y = display.contentHeight - 490} 
+        }
+
+    -- Criação dos vírus e bactérias
+    for i, pos in ipairs(posiVirus) do
+        virusArray[i] = criarEntidade(sceneGroup, "assets/virus.png", pos.x, pos.y)
+       
+    end
+    for i, pos in ipairs(posiBac) do
+        bacArray[i] = criarEntidade(sceneGroup, "assets/bacteria.png", pos.x, pos.y)
+    end
+
+    -- Carregar o som do botão e colisão
+    somBotao = audio.loadSound("assets/som.mp3")
+    somColisao = audio.loadSound("assets/somDestruir.mp3")
+
+    -- Função para verificar colisões em cada frame
+    local function atualizar()
+        atualizarColisao(zeGotinha)
+    end
+
+    Runtime:addEventListener("enterFrame", atualizar)
+
+    -- Função para navegar para a próxima página
+    local function onNextTap(event)
+        audio.play(somBotao)
+        composer.gotoScene("page2", { effect = "slideLeft", time = 500 })
+    end
+
+    -- Função para navegar para a página anterior
     local function onBackTap(event)
         audio.play(somBotao)
         composer.gotoScene("capa", { effect = "slideRight", time = 500 })
     end
 
-       -- Função para navegar para a continuação da  pagina
-    local function onContinueTap(event)
-        audio.play(somBotao)
-        composer.gotoScene("page2", { effect = "slideLeft", time = 500 })
-    end
-    
-    --- add os botoes 
-
-    -- botão 'Continuação da pagina '
-    local btContinua = createButton(
-        sceneGroup,
-        "assets/btContinua.png",
-        display.contentWidth - 110, 
-        display.contentHeight - 200, 
-        0.7, 
-        0.7, 
-        onContinueTap
-    )
-
+    -- Criação dos botões de navegação e som
     -- botão 'Próximo'
     local btProx = createButton(
         sceneGroup,
@@ -109,7 +179,7 @@ function scene:create(event)
         onNextTap 
     )
 
-    --  botão 'Voltar'
+    -- botão 'Voltar'
     local btVolt = createButton(
         sceneGroup,
         "assets/bt-voltar.png",
@@ -120,8 +190,8 @@ function scene:create(event)
         onBackTap 
     )
 
-      -- botão 'Ligar Som'
-      local btSomL = createButton(
+    -- botão 'Ligar Som'
+    local btSomL = createButton(
         sceneGroup,
         "assets/som-ligar.png",
         display.contentWidth - 530,
@@ -141,56 +211,37 @@ function scene:create(event)
         0.5, 
         onSoundOffTap 
     )
-
-    
 end
 
--- show()
+-- Funções show, hide e destroy
 function scene:show(event)
-    local sceneGroup = self.view
     local phase = event.phase
-
-    if (phase == "will") then
-        -- Código aqui é executado quando a cena está prestes a aparecer na tela
-
-    elseif (phase == "did") then
-        -- Código aqui é executado quando a cena já está na tela
-
+    if phase == "did" then
+        -- Quando a cena se torna visível
     end
 end
 
--- hide()
 function scene:hide(event)
-    local sceneGroup = self.view
     local phase = event.phase
-
-    if (phase == "will") then
-        -- Código aqui é executado quando a cena está prestes a sair da tela
-
-    elseif (phase == "did") then
-        -- Código aqui é executado imediatamente após a cena sair da tela
-
+    if phase == "did" then
+        -- Quando a cena sai da tela
     end
 end
 
--- destroy()
 function scene:destroy(event)
-    local sceneGroup = self.view
-    -- Liberar o som quando a cena for destruída
     if somBotao then
-        audio.stop()
         audio.dispose(somBotao)
-        somBotao= nil
+        somBotao = nil
+    end
+    if somColisao then
+        audio.dispose(somColisao)
+        somColisao = nil
     end
 end
 
--- -----------------------------------------------------------------------------------
--- Scene event function listeners
--- -----------------------------------------------------------------------------------
 scene:addEventListener("create", scene)
 scene:addEventListener("show", scene)
 scene:addEventListener("hide", scene)
 scene:addEventListener("destroy", scene)
--- -----------------------------------------------------------------------------------
 
 return scene
